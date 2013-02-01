fb003766f189ca39dea05cbbaaf46d977b1cc514
Josh Elkin <josh.elkin@gmail.com>
2013-02-01 10:02:00 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandoomi::Application.config.secret_token = 'be9e484633795682a16bfd3c19adda7da99300137de0d8f5042c7eeefdc606941c093feae9c98337742cab61d878e42b5f4ee9aac487dbf29b25172152fb553f'
